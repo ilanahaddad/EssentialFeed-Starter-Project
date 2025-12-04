b5cb568a291a937d81473a0b0c172e01821d04d1
Ilana Haddad <ilana.haddad@lightspeedhq.com>
2025-12-04 07:11:57 -0500

    make RemoteFeedLoader conform to FeedLoader protocol

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // final: don't allow subclasses of this type
-public final class RemoteFeedLoader {
+public final class RemoteFeedLoader: FeedLoader {
     private let url: URL
     private let client: HTTPClient
     
