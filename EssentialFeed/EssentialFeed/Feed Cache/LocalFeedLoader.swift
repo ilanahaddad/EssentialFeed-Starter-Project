4d45e5eac681e87e278bea49446d0e0f936a9214
Ilana Haddad <ilana.haddad@lightspeedhq.com>
2025-12-27 18:29:10 -0500

    Add LocalFeedItem Data transfer representation to decouple storage frameworks from FeedItem data models

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -31,10 +31,16 @@ public class LocalFeedLoader {
     
     private func cache(_ items: [FeedItem], with completion: @escaping (SaveResult) -> Void) {
         store.insert(
-            items,
+            items.toLocal(),
             timestamp: self.currentDate()) { [weak self] cacheInsertionError in
                 guard self != nil else { return }
                 completion(cacheInsertionError)
             }
+    }
+}
+
+private extension Array where Element == FeedItem {
+    func toLocal() -> [LocalFeedItem] {
+        return map { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL)}
     }
 }
