e1994d91930df7ac000d64b3eefef4971509e574
Ilana Haddad <ilana.haddad@lightspeedhq.com>
2025-12-29 10:31:44 -0500

    extract duplicate test helpers into a shared scope

--- EssentialFeed/EssentialFeedTests/Helpers/XCTTestCase+MemoryLeakTracking.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/XCTTestCase+MemoryLeakTracking.swift
@@ -19,15 +19,7 @@ extension XCTestCase {
         }
     }
     
-    func anyURL() -> URL {
-        return URL(string: "http://any-url.com")!
-    }
-    
     func anyData() -> Data {
         return Data("any data".utf8)
-    }
-    
-    func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0)
     }
 }
